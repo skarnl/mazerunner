9a11ea7d28f063687ac88ccf949a501f3f46cd9d
Oskar van Velden <oskar@zicht.nl>
2016-02-19 13:38:28 +0100

    refactored detection

--- src/Application.js
+++ src/Application.js
@@ -62,14 +62,13 @@ export class Application
 
 	startSequence ()
 	{
-		this.findMazeTopLeftCorner();
-
 		this.findDimentionsOfMaze();
+
 		// this.lookForEntrance();
 		// this.spawnWorker();
 	}
 
-	findMazeTopLeftCorner ()
+	findTopLeftCorner ()
 	{
 		find_top_corner_loop:
 		for( let iy = 0; iy < this.img.height; iy++ ) {
@@ -77,54 +76,99 @@ export class Application
 
 				if (!this.isWhite(ix , iy)) {
 					this.topLeftCorner = new Point(ix, iy);
-					
-					console.log('Found topLeftCorner: ' + this.topLeftCorner);
 					break find_top_corner_loop;
 				}
 			}
 		}
+
+		console.log('Found topLeftCorner: ' + this.topLeftCorner);
+
+		this.drawPixel(this.topLeftCorner);
+	}
+
+	findTopRightCorner()
+	{
+		let currentY = this.topLeftCorner.y;
+
+		for (let x = this.img.width; x > this.topLeftCorner.x; x--) {
+			if (this.isBlack(x, currentY)) {
+				this.topRightCorner = new Point(x, currentY);
+				break;
+			}
+		}
+
+		console.log('Found topRightCorner: ' + this.topRightCorner);
+		this.drawPixel(this.topRightCorner);
+	}
+
+	findBottomRightCorner()
+	{
+		let currentX = this.topRightCorner.x;
+
+		for (let y = this.img.height; y > this.topRightCorner.y; y--) {
+			if (this.isBlack(currentX, y)) {
+				this.bottomRightCorner = new Point(currentX, y);
+				break;
+			}
+		}
+
+		console.log('Found bottomRightCorner: ' + this.bottomRightCorner);
+		this.drawPixel(this.bottomRightCorner);
+	}
+
+	defineBottomLeftCorner ()
+	{
+		this.bottomLeftCorner = new Point(this.topLeftCorner.x, this.bottomRightCorner.y);
+
+		console.log('bottomLeftCorner defined: ' + this.bottomLeftCorner);
+		this.drawPixel(this.bottomLeftCorner);
 	}
 
 	findDimentionsOfMaze ()
 	{
-		this.entrance = new Doorway();
-		this.exit = new Doorway();
-		
-		//horizontal
-		for ( let ix = this.topLeftCorner.x; ix < this.img.width; ix++) {
-			
-			if (this.isWhite(ix, this.topLeftCorner.y)) {
-				if (this.entrance.getLeftPost() && this.entrance.getRightPost()) {
-					this.topRightCorner = new Point(ix, this.topLeftCorner.y);
-					break;
-				}
+		this.findTopLeftCorner();
+		this.findTopRightCorner();
+		this.findBottomRightCorner();
+		this.defineBottomLeftCorner();
 
+		this.findEntrance();
+	}
+
+	findEntrance ()
+	{
+		console.log('- findEntrance');
+		
+		this.entrance = new Doorway();
+
+		let currentX = this.topLeftCorner.x,
+			currentY = this.topLeftCorner.y,
+			previousX,
+			previousY;
+
+		while( !this.entrance.isDefined() ) {
+			if(this.isWhite(currentX, currentY)) {
 				if (this.entrance.getLeftPost() === null) {
-					this.entrance.setLeftPost(ix - 1, this.topLeftCorner.y);
+					//Math.min -> so we don't need to remember if we are going horizontal or vertical
+					this.entrance.setLeftPost(Math.min(currentX, previousX), Math.min(currentY, previousY));
 				}
 			} else { //black
 				if (this.entrance.getLeftPost() && this.entrance.getRightPost() === null) {
-					this.entrance.setRightPost(ix, this.topLeftCorner.y);
+					this.entrance.setRightPost(currentX, currentY);
 				}
+			}
+
+			currentX++;
+
+			if (currentX > this.topRightCorner.x) {
+				currentX = this.topRightCorner.x;
+				currentY++;
+			}
+
+			if (currentY > this.bottomRightCorner.y) {
+				currentY = this.bottomRightCorner.y;
+				currentX--;
 			}
 		}
-
-		/*for (let iy = this.topLeftCorner.y; iy < this.img.height; iy++ )) {
-			if (this.isWhite(this.topRightCorner.x, iy)) {
-				if (this.entrance.getLeftPost() && this.entrance.getRightPost() ) {
-					this.bottomRightCorner = new Point(this.topRightCorner.x, iy);
-					break;
-				}
-
-				if (this.entrance.getLeftPost() === null) {
-					this.entrance.setLeftPost(ix - 1, this.topLeftCorner.y);
-				}
-			} else { //black
-				if (this.entrance.getLeftPost() && this.entrance.getRightPost() === null) {
-					this.entrance.setRightPost(ix, this.topLeftCorner.y);
-				}
-			}
-		}*/
 	}
 
 	getPixel (x, y)
@@ -138,6 +182,11 @@ export class Application
 		return pixelData[0] === 255 && pixelData[1] === 255 && pixelData[2] === 255;
 	}
 
+	isBlack (x, y)
+	{
+		return !this.isWhite(x, y);
+	}
+
 	spawnWorker ()
 	{
 		
@@ -147,13 +196,19 @@ export class Application
 		w.work();
 	}
 
-	drawPixel (startPoint, endPoint)
+	drawPixel (startPoint, endPoint = undefined)
 	{
+		if (endPoint === undefined) {
+			endPoint = startPoint;
+			endPoint.x = endPoint.x + 1;
+			endPoint.y = endPoint.y + 1;
+		}
+
 		this.context.beginPath();
-		this.context.lineWidth = 1;
+		this.context.lineWidth = 3;
 
 		// set line color
-		this.context.strokeStyle = '#E60E7D';
+		this.context.strokeStyle = '#FF0000';
   		this.context.moveTo(startPoint.x, startPoint.y);
   		this.context.lineTo(endPoint.x, endPoint.y);
   		this.context.stroke();
